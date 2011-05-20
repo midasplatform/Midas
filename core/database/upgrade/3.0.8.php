57b71e6395fb41e702d78756edec6605c32e4b93
Charles Marion <charles.marion@kitware.com>
2011-05-20 09:47:50 +0200

    BUG : fixed upgrade/3.0.8.php

--- core/database/upgrade/3.0.8.php
+++ core/database/upgrade/3.0.8.php
@@ -10,6 +10,7 @@ class Upgrade_3_0_8 extends MIDASUpgrade
   public function mysql()
     {
     $sql = "ALTER TABLE folder ADD COLUMN privacy_status  integer DEFAULT 0; ";
+    $this->db->query($sql);
     $sql = "ALTER TABLE item ADD COLUMN privacy_status  integer DEFAULT 0; ";
     $this->db->query($sql);
     }
@@ -17,6 +18,7 @@ class Upgrade_3_0_8 extends MIDASUpgrade
   public function pgsql()
     {
     $sql = "ALTER TABLE folder ADD COLUMN privacy_status  integer NOT NULL  DEFAULT '0' ; ";
+    $this->db->query($sql);
     $sql = "ALTER TABLE item ADD COLUMN privacy_status  integer NOT NULL  DEFAULT '0' ; ";
     $this->db->query($sql);
     }
