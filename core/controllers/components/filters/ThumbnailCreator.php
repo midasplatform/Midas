446ce263cde1809b8b02c1e3c44f8450078ec60f
Charles Marion <charles.marion@kitware.com>
2011-05-25 15:33:21 +0200

    BUG: disabled video ThumbnailCreator.php

--- core/controllers/components/filters/ThumbnailCreator.php
+++ core/controllers/components/filters/ThumbnailCreator.php
@@ -106,7 +106,7 @@ class ThumbnailCreator extends AppFilters
       case "mha":
       case "nrrd":
       case "":
-        $ret = $this->_processMetaImage($extension);
+        //$ret = $this->_processMetaImage($extension);
         break;
       case "pdf":
         $this->inputFile .= "[0]";    // first page only
@@ -120,7 +120,7 @@ class ThumbnailCreator extends AppFilters
       case "flv":
       case "mp4":
       case "rm":
-        $ret = $this->_processVideo();
+        //$ret = $this->_processVideo();
         break;
       default:
         $ret = $this->_processStandard();
