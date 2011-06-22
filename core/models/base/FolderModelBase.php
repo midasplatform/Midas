2d3e9f31c72f121469d0e7d79fd1a019f024043b
Charles Ma <charles.marion@kitware.com>
2011-06-22 16:49:15 +0200

    BUG: fixed bug #142 Folders and items shouldn t have the same name and same parent

--- core/models/base/FolderModelBase.php
+++ core/models/base/FolderModelBase.php
@@ -54,7 +54,7 @@ abstract class FolderModelBase extends AppModel
   abstract function move($folder, $parent);
   abstract function removeItem($folder, $item);
   abstract function policyCheck($folderDao, $userDao = null, $policy = 0);
-  abstract function getFolderExists($name, $description);
+  abstract function getFolderExists($name, $parent);
   abstract function getByUuid($uuid);
   abstract function getRoot($folder);
   
@@ -109,9 +109,9 @@ abstract class FolderModelBase extends AppModel
       }  
 
     // Check ifa folder with the same name already exists for the same parent
-    if($this->getFolderExists($name, $parentId))
+    if($this->getFolderExists($name, $parent))
       {
-      $existingfolder = $this->getFolderExists($name, $parentId);
+      $existingfolder = $this->getFolderExists($name, $parent);
       return $existingfolder;
       }
       
