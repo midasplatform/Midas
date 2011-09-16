cf52fc06d32ae5b66e8e2392385b982bba200869
Michael Grauer <michael.grauer@kitware.com>
2011-09-16 18:04:44 -0400

    BUG: Refs #212.  Rewrite of KWBatchmakeComponent.php, and reworking of tests.
    Rewrote KWBatchmakeComponent.php to be more sensible, rewrote tests and other
    code that depending on this.  Also added a README to the batchmake module for
    testing, and a switch in CMakeLists.txt for the batchmake module to have
    batchmake module testing be off by default, since a certain amount of config
    is required to have the batchmake tests pass.

--- modules/batchmake/tests/controllers/components/KWBatchmakeComponentTest.php
+++ modules/batchmake/tests/controllers/components/KWBatchmakeComponentTest.php
@@ -27,9 +27,7 @@ class KWBatchmakeComponentTest extends ControllerTestCase
     // need to include the module constant for this test  
     require_once BASE_PATH.'/modules/batchmake/constant/module.php';
     require_once BASE_PATH.'/modules/batchmake/controllers/components/KWBatchmakeComponent.php';
-    $this->kwBatchmakeComponent = new Batchmake_KWBatchmakeComponent();      
-    $this->kwBatchmakeComponent->setAlternateConfig(BASE_PATH.'/modules/batchmake/tests/configs/module.local.ini');
-    $this->applicationConfig = $this->kwBatchmakeComponent->getApplicationConfigProperties();
+    $this->kwBatchmakeComponent = new Batchmake_KWBatchmakeComponent(BASE_PATH.'/modules/batchmake/tests/configs/module.local.ini');      
     }
     
   /** set up tests*/
@@ -41,19 +39,18 @@ class KWBatchmakeComponentTest extends ControllerTestCase
     parent::setUp();
     }
 
-    
-  // TEST KWBATCHMAKE COMPONENTS
-  // want a better way of doing this, but here is a start
-    
   /**
    * tests config setup, relies on an alternate testing config to be defined,
    * these properties should all point to the batchmake module testfiles dirs.
    */
-  // should put the kwBatchmakeComponent as an instance variable?
-  // hopefully that would remove all of the redundant calls to setting it up
   public function testIsConfigCorrect()
     {
     $this->assertTrue($this->kwBatchmakeComponent->isConfigCorrect());
+    // start out with know correct set
+    $badConfigVals = $this->kwBatchmakeComponent->loadConfigProperties(BASE_PATH.'/modules/batchmake/tests/configs/module.local.ini');
+    // change a value to something bad
+    $badConfigVals[MIDAS_BATCHMAKE_DATA_DIR_PROPERTY] = '/unlikely/to/work/right';
+    $this->assertFalse($this->kwBatchmakeComponent->isConfigCorrect($badConfigVals));
     }  
   
   /**
@@ -73,13 +70,6 @@ class KWBatchmakeComponentTest extends ControllerTestCase
     }
     
   /**
-   * @TODO some model testing, and better ways of loading the models
-   * test creating a model and creating the right subdirs
-   */ 
-
- 
-   
-  /**
    * helper function to clear out any files in a directory
    */
   protected function clearDirFiles($dirToClear)
@@ -97,13 +87,13 @@ class KWBatchmakeComponentTest extends ControllerTestCase
   /**
    * helper function to run a test case 
    */ 
-  protected function fASLDBSWCDtestcase($scriptDir, $tmpDir, $scriptName, $expectedSet)
+  protected function preparePipelineScriptsTestcase($workDir, $scriptName, $expectedSet)
     {
     // clear the directory of any existing files
-    $this->clearDirFiles($tmpDir);  
+    $this->clearDirFiles($workDir);  
     
     // try symlinking all the batchmake files starting with $scriptName
-    $bmScriptsProcessed = $this->kwBatchmakeComponent->findAndSymLinkDependentBatchmakeScriptsWithCycleDetection($scriptDir, $tmpDir, $scriptName);
+    $bmScriptsProcessed = $this->kwBatchmakeComponent->preparePipelineScripts($workDir, $scriptName);
     
     // check that the correct batchmake scripts are there, and only those
     // easiest just to add '.' and '..' to expected list
@@ -111,7 +101,7 @@ class KWBatchmakeComponentTest extends ControllerTestCase
     $expectedSet[] = '.';
     sort($expectedSet);
 
-    $foundScripts = scandir($tmpDir);
+    $foundScripts = scandir($workDir);
     sort($foundScripts);
     
     $this->assertEquals($expectedSet, $foundScripts, 
@@ -130,12 +120,12 @@ class KWBatchmakeComponentTest extends ControllerTestCase
   /**
    * helper function to run a test case that is expected to throw an exception 
    */ 
-  protected function fASLDBSWCDtestcaseException($scriptDir, $tmpDir, $scriptName)
+  protected function preparePipelineScriptsTestcaseException($workDir, $scriptName)
     {
     try
       {
       // need to suppress error output to keep test from failing, despite exception being caught
-      $bmScripts = @$this->kwBatchmakeComponent->findAndSymLinkDependentBatchmakeScriptsWithCycleDetection($scriptDir, $tmpDir, $scriptName);
+      $bmScripts = @$this->kwBatchmakeComponent->preparePipelineScripts($workDir, $scriptName);
       $this->fail('Expected an exception for $scriptName, but did not get one.');
       }  
     catch(Zend_Exception $ze)
@@ -146,38 +136,26 @@ class KWBatchmakeComponentTest extends ControllerTestCase
     }
     
     
-  /** tests findAndSymLinkDependentBatchmakeScriptsWithCycleDetection. */
-  public function testFindAndSymLinkDependentBatchmakeScriptsWithCycleDetection()
+  /** tests preparePipelineScripts, and exercises createTask. */
+  public function testPreparePipelineScripts()
     {
-    $applicationConfig = $this->kwBatchmakeComponent->getApplicationConfigProperties();
-    // create a task
-    $modelLoad = new MIDAS_ModelLoader();
-    $batchmakeTaskModel = $modelLoad->loadModel('Task', 'batchmake');
     $usersFile = $this->loadData('User', 'default');
     $userDao = $this->User->load($usersFile[0]->getKey());
-    $taskDao = $batchmakeTaskModel->createTask($userDao);
-    $userId = $taskDao->getUserId();
-    $taskId = $taskDao->getKey();
-    $subdirs = array(MIDAS_BATCHMAKE_SSP_DIR, $userId, $taskId);
-    // create a tmpDir based on the task and user
-    $tmpDir = KWUtils::createSubDirectories($applicationConfig[MIDAS_BATCHMAKE_TMP_DIR_PROPERTY] . "/", $subdirs);
-
-    $scriptDir = $applicationConfig[MIDAS_BATCHMAKE_SCRIPT_DIR_PROPERTY];
+    $workDir = $this->kwBatchmakeComponent->createTask($userDao);  
     
-  
     $scriptName = 'anotherscript.bms';
     $expectedSet = array("myscript.bms", "Myscript2.bms",
          "anotherscript.bms", "noscripts.bms", "PixelCounter.bms");
-    $this->fASLDBSWCDtestcase($scriptDir, $tmpDir, $scriptName, $expectedSet);
+    $this->preparePipelineScriptsTestcase($workDir, $scriptName, $expectedSet);
    
     $scriptName = "noscripts.bms";
     $expectedSet = array("noscripts.bms");
-    $this->fASLDBSWCDtestcase($scriptDir, $tmpDir, $scriptName, $expectedSet);
+    $this->preparePipelineScriptsTestcase($workDir, $scriptName, $expectedSet);
     
     // try symlinking all the batchmake files starting with anotherscriptwitherrors.bms
     // expect an exception, as this script includes a non-existent script
     $scriptName = 'anotherscriptwitherrors.bms';
-    $this->fASLDBSWCDtestcaseException($scriptDir, $tmpDir, $scriptName);
+    $this->preparePipelineScriptsTestcaseException($workDir, $scriptName);
     
     // cycle detection tests
     
@@ -185,44 +163,44 @@ class KWBatchmakeComponentTest extends ControllerTestCase
     // clear the directory of the symlinked files
     $scriptName = "nocycle1.bms";
     $expectedSet = array("nocycle1.bms", "nocycle2.bms", "nocycle3.bms");
-    $this->fASLDBSWCDtestcase($scriptDir, $tmpDir, $scriptName, $expectedSet);
+    $this->preparePipelineScriptsTestcase($workDir, $scriptName, $expectedSet);
   
     // expect an exception, as this script has a simple cycle
     // 1->1
     $scriptName = 'cycle1.bms';
-    $this->fASLDBSWCDtestcaseException($scriptDir, $tmpDir, $scriptName);
+    $this->preparePipelineScriptsTestcaseException($workDir, $scriptName);
     
     // check a script with a more complex cycle, 1->2, 1->3, 2->3, 3->2
     $scriptName = 'cycle31.bms';
-    $this->fASLDBSWCDtestcaseException($scriptDir, $tmpDir, $scriptName);      
+    $this->preparePipelineScriptsTestcaseException($workDir, $scriptName);      
     }
     
     
-  /** tests findAndSymLinkDependentBmms */
-  public function testFindAndSymLinkDependentBmms()
+  /** tests preparePipelineBmms */
+  public function testPreparePipelineBmms()
     {
-    $applicationConfig = $this->kwBatchmakeComponent->getApplicationConfigProperties();
-    // create a task
-    $modelLoad = new MIDAS_ModelLoader();
-    $batchmakeTaskModel = $modelLoad->loadModel('Task', 'batchmake');
     $usersFile = $this->loadData('User', 'default');
     $userDao = $this->User->load($usersFile[0]->getKey());
-    $taskDao = $batchmakeTaskModel->createTask($userDao);
-    $userId = $taskDao->getUserId();
-    $taskId = $taskDao->getKey();
-    $subdirs = array(MIDAS_BATCHMAKE_SSP_DIR, $userId, $taskId);
-    // create a tmpDir based on the task and user
-    $tmpDir = KWUtils::createSubDirectories($applicationConfig[MIDAS_BATCHMAKE_TMP_DIR_PROPERTY] . "/", $subdirs);
+    $workDir = $this->kwBatchmakeComponent->createTask($userDao);      
     
-    $scriptDir = $applicationConfig[MIDAS_BATCHMAKE_SCRIPT_DIR_PROPERTY];
-    $appDir = $applicationConfig[MIDAS_BATCHMAKE_APP_DIR_PROPERTY];
+    // try a script that refers to a non-existant bmm
+    $scriptName = 'bmmswitherrors.bms';  
+    $bmScripts = $this->kwBatchmakeComponent->preparePipelineScripts($workDir, $scriptName);
+    try
+      {
+      $bmms = $this->kwBatchmakeComponent->preparePipelineBmms($workDir, $bmScripts);
+      $this->fail('Expected an exception for '.$scriptName.', but did not get one.');
+      }  
+    catch(Zend_Exception $ze)
+      {
+      // if we got here, this is the correct behavior
+      $this->assertTrue(true);
+      }
     
     // now try symlinking all the batchmake files starting with anotherscript.bms
     $scriptName = 'anotherscript.bms';
-    $bmScripts_anotherscript = $this->kwBatchmakeComponent->findAndSymLinkDependentBatchmakeScriptsWithCycleDetection($scriptDir, $tmpDir, $scriptName);
- 
-    
-    $bmms = $this->kwBatchmakeComponent->findAndSymLinkDependentBmms($appDir, $tmpDir, $bmScripts_anotherscript);
+    $bmScripts_anotherscript = $this->kwBatchmakeComponent->preparePipelineScripts($workDir, $scriptName);    
+    $bmms = $this->kwBatchmakeComponent->preparePipelineBmms($workDir, $bmScripts_anotherscript);
     // these come as [ name of app => script where found ]
     // convert them to a form useful for comparison
     $processedBmms_anotherscript = array();
@@ -232,7 +210,7 @@ class KWBatchmakeComponentTest extends ControllerTestCase
       }
     sort($processedBmms_anotherscript);
     
-    $globOutput = glob($tmpDir.'/*.bmm');
+    $globOutput = glob($workDir.'/*.bmm');
     // strip off the path
     $foundBmms_anotherscript = array();
     foreach($globOutput as $bmm) 
@@ -248,7 +226,6 @@ class KWBatchmakeComponentTest extends ControllerTestCase
     // compare the three arrays
     $this->assertEquals($processedBmms_anotherscript, $expectedBmms_anotherscript, "BMMs: processed != expected, for anotherscript.bms");
     $this->assertEquals($processedBmms_anotherscript, $foundBmms_anotherscript, "BMMs: processed != found, for anotherscript.bms");
-  
     }
     
     
@@ -260,51 +237,25 @@ class KWBatchmakeComponentTest extends ControllerTestCase
   /** tests testCompileBatchMakeScript */
   public function testCompileBatchMakeScript()
     {
-    // create a task
-    $modelLoad = new MIDAS_ModelLoader();
-    $batchmakeTaskModel = $modelLoad->loadModel('Task', 'batchmake');
     $usersFile = $this->loadData('User', 'default');
     $userDao = $this->User->load($usersFile[0]->getKey());
-    $taskDao = $batchmakeTaskModel->createTask($userDao);
-    $userId = $taskDao->getUserId();
-    $taskId = $taskDao->getKey();
-    $subdirs = array(MIDAS_BATCHMAKE_SSP_DIR, $userId, $taskId);
-    // create a tmpDir based on the task and user
-    $applicationConfig = $this->kwBatchmakeComponent->getApplicationConfigProperties();
-    $tmpDir = KWUtils::createSubDirectories($applicationConfig[MIDAS_BATCHMAKE_TMP_DIR_PROPERTY] . "/", $subdirs);
+    $workDir = $this->kwBatchmakeComponent->createTask($userDao);  
     
-    $scriptDir = $applicationConfig[MIDAS_BATCHMAKE_SCRIPT_DIR_PROPERTY];
-    $binDir = $applicationConfig[MIDAS_BATCHMAKE_BIN_DIR_PROPERTY];
-    $appDir = $applicationConfig[MIDAS_BATCHMAKE_APP_DIR_PROPERTY];
-      
     // a script that compiles
     $scriptName = 'Compiles.bms';
-    $bmScripts = $this->kwBatchmakeComponent->findAndSymLinkDependentBatchmakeScriptsWithCycleDetection($scriptDir, $tmpDir, $scriptName);
-    $bmms = $this->kwBatchmakeComponent->findAndSymLinkDependentBmms($appDir, $tmpDir, $bmScripts);
+    $bmScripts = $this->kwBatchmakeComponent->preparePipelineScripts($workDir, $scriptName);
+    $bmms = $this->kwBatchmakeComponent->preparePipelineBmms($workDir, $bmScripts);
 
-    // first try with a bad path to BatchMake
-    $badBinDir = '/a/dir/not/likely/to/exist';
-    try
-      {
-      $this->kwBatchmakeComponent->compileBatchMakeScript($appDir, $badBinDir, $tmpDir, $scriptName);
-      $this->fail('Should have not been able to find BatchMake, but did,  testCompileBatchMakeScript');
-      }
-    catch(Zend_Exception $ze)
-      {
-      // if we got here, this is the correct behavior
-      $this->assertTrue(true);
-      }   
-    
     // this one should work
-    $this->kwBatchmakeComponent->compileBatchMakeScript($appDir, $binDir, $tmpDir, $scriptName);
+    $this->kwBatchmakeComponent->compileBatchMakeScript($workDir, $scriptName);
 
     // now try a script that doesn't compile
     $scriptName = 'CompileErrors.bms';
-    $bmScripts = $this->kwBatchmakeComponent->findAndSymLinkDependentBatchmakeScriptsWithCycleDetection($scriptDir, $tmpDir, $scriptName);
-    $bmms = $this->kwBatchmakeComponent->findAndSymLinkDependentBmms($appDir, $tmpDir, $bmScripts);
+    $bmScripts = $this->kwBatchmakeComponent->preparePipelineScripts($workDir, $scriptName);
+    $bmms = $this->kwBatchmakeComponent->preparePipelineBmms($workDir, $bmScripts);
     try
       {
-      $this->kwBatchmakeComponent->compileBatchMakeScript($appDir, $binDir, $tmpDir, $scriptName);
+      $this->kwBatchmakeComponent->compileBatchMakeScript($workDir, $scriptName);
       $this->fail('Should have had a compile error but did not, testCompileBatchMakeScript');
       }
     catch(Zend_Exception $ze)
@@ -318,39 +269,26 @@ class KWBatchmakeComponentTest extends ControllerTestCase
   /** tests generateCondorDag */
   public function testGenerateCondorDag()
     {
-    // create a task
-    $modelLoad = new MIDAS_ModelLoader();
-    $batchmakeTaskModel = $modelLoad->loadModel('Task', 'batchmake');
     $usersFile = $this->loadData('User', 'default');
     $userDao = $this->User->load($usersFile[0]->getKey());
-    $taskDao = $batchmakeTaskModel->createTask($userDao);
-    $userId = $taskDao->getUserId();
-    $taskId = $taskDao->getKey();
-    $subdirs = array(MIDAS_BATCHMAKE_SSP_DIR, $userId, $taskId);
-    // create a tmpDir based on the task and user
-    $applicationConfig = $this->kwBatchmakeComponent->getApplicationConfigProperties();
-    $tmpDir = KWUtils::createSubDirectories($applicationConfig[MIDAS_BATCHMAKE_TMP_DIR_PROPERTY] . "/", $subdirs);
+    $workDir = $this->kwBatchmakeComponent->createTask($userDao);  
     
-    $scriptDir = $applicationConfig[MIDAS_BATCHMAKE_SCRIPT_DIR_PROPERTY];
-    $binDir = $applicationConfig[MIDAS_BATCHMAKE_BIN_DIR_PROPERTY];
-    $appDir = $applicationConfig[MIDAS_BATCHMAKE_APP_DIR_PROPERTY];
-      
     // a script that compiles
     $scriptName = 'Compiles.bms';
-    $bmScripts = $this->kwBatchmakeComponent->findAndSymLinkDependentBatchmakeScriptsWithCycleDetection($scriptDir, $tmpDir, $scriptName);
-    $bmms = $this->kwBatchmakeComponent->findAndSymLinkDependentBmms($appDir, $tmpDir, $bmScripts);
+    $bmScripts = $this->kwBatchmakeComponent->preparePipelineScripts($workDir, $scriptName);
+    $bmms = $this->kwBatchmakeComponent->preparePipelineBmms($workDir, $bmScripts);
     
     // try to generate the Condor script
-    $dagJobFile = $this->kwBatchmakeComponent->generateCondorDag($appDir, $tmpDir, $binDir, $scriptName);
+    $dagJobFile = $this->kwBatchmakeComponent->generateCondorDag($workDir, $scriptName);
     $this->assertEquals($dagJobFile, 'Compiles.bms.dagjob');
     // check that dag files and condor job files were created
     $condorFiles = array($dagJobFile, 'Compiles.1.bms.dagjob', 'Compiles.3.bms.dagjob', 'Compiles.5.bms.dagjob');
     foreach($condorFiles as $condorFile)
       {
-      $this->assertFileExists($tmpDir.'/'.$condorFile);
+      $this->assertFileExists($workDir.'/'.$condorFile);
       }
     // now look for some specific strings
-    $contents = file_get_contents($tmpDir.'/'. 'Compiles.bms.dagjob');
+    $contents = file_get_contents($workDir.'/'. 'Compiles.bms.dagjob');
     $dagjobStrings = array('Job job3', 'Job job5', 'PARENT job1 CHILD job3', 'PARENT job3 CHILD job5');
     foreach($dagjobStrings as $string)
       { 
@@ -362,46 +300,19 @@ class KWBatchmakeComponentTest extends ControllerTestCase
   /** tests function testCondorSubmitDag */
   public function testCondorSubmitDag()
     {
-    // create a task
-    $modelLoad = new MIDAS_ModelLoader();
-    $batchmakeTaskModel = $modelLoad->loadModel('Task', 'batchmake');
     $usersFile = $this->loadData('User', 'default');
     $userDao = $this->User->load($usersFile[0]->getKey());
-    $taskDao = $batchmakeTaskModel->createTask($userDao);
-    $userId = $taskDao->getUserId();
-    $taskId = $taskDao->getKey();
-    $subdirs = array(MIDAS_BATCHMAKE_SSP_DIR, $userId, $taskId);
-    // create a tmpDir based on the task and user
-    $applicationConfig = $this->kwBatchmakeComponent->getApplicationConfigProperties();
-    $tmpDir = KWUtils::createSubDirectories($applicationConfig[MIDAS_BATCHMAKE_TMP_DIR_PROPERTY] . "/", $subdirs);
-    
-    $scriptDir = $applicationConfig[MIDAS_BATCHMAKE_SCRIPT_DIR_PROPERTY];
-    $binDir = $applicationConfig[MIDAS_BATCHMAKE_BIN_DIR_PROPERTY];
-    $appDir = $applicationConfig[MIDAS_BATCHMAKE_APP_DIR_PROPERTY];
-    $condorBinDir = $applicationConfig[MIDAS_BATCHMAKE_CONDOR_BIN_DIR_PROPERTY];
+    $workDir = $this->kwBatchmakeComponent->createTask($userDao);  
       
     // a script that compiles
     $scriptName = 'Compiles.bms';
-    $bmScripts = $this->kwBatchmakeComponent->findAndSymLinkDependentBatchmakeScriptsWithCycleDetection($scriptDir, $tmpDir, $scriptName);
-    $bmms = $this->kwBatchmakeComponent->findAndSymLinkDependentBmms($appDir, $tmpDir, $bmScripts);
+    $bmScripts = $this->kwBatchmakeComponent->preparePipelineScripts($workDir, $scriptName);
+    $bmms = $this->kwBatchmakeComponent->preparePipelineBmms($workDir, $bmScripts);
 
-    $dagScript = $this->kwBatchmakeComponent->generateCondorDag($appDir, $tmpDir, $binDir, $scriptName);
-    $this->kwBatchmakeComponent->condorSubmitDag($condorBinDir, $tmpDir, $dagScript);
+    $dagScript = $this->kwBatchmakeComponent->generateCondorDag($workDir, $scriptName);
+    $this->kwBatchmakeComponent->condorSubmitDag($workDir, $dagScript);
     // how to check this now?
     }
 
 
-
-    
-    
-
-
-    
-
-
-    
-   
-    
-    
-    
   } // end class
