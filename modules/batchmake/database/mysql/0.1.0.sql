0eb51666c8265a0d6ee91ece9d97a5e1530b9687
Michael Grauer <michael.grauer@kitware.com>
2011-09-22 19:39:05 -0400

    ENH: refs #0258.  Added Itemmetric models and tests, and tests for Task model.

--- modules/batchmake/database/mysql/0.1.0.sql
+++ modules/batchmake/database/mysql/0.1.0.sql
@@ -3,3 +3,11 @@ CREATE TABLE batchmake_task (
     user_id bigint(20) NOT NULL,
     PRIMARY KEY (batchmake_task_id)
 );
+
+
+CREATE TABLE batchmake_itemmetric (
+    itemmetric_id bigint(20) NOT NULL AUTO_INCREMENT,
+    metric_name character varying(64) NOT NULL,
+    bms_name character varying(256) NOT NULL,
+    PRIMARY KEY (itemmetric_id)
+);
---
+++ modules/batchmake/models/dao/ItemmetricDao.php
@@ -0,0 +1,20 @@
+<?php
+/*=========================================================================
+MIDAS Server
+Copyright (c) Kitware SAS. 20 rue de la Villette. All rights reserved.
+69328 Lyon, FRANCE.
+
+See Copyright.txt for details.
+This software is distributed WITHOUT ANY WARRANTY; without even
+the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+PURPOSE.  See the above copyright notices for more information.
+=========================================================================*/
+/** Batchmake_ItemmetricDao */
+class Batchmake_ItemmetricDao extends AppDao {
+
+  public $_model = 'Itemmetric';
+  public $_module = 'batchmake';
+
+}
+
+?>
