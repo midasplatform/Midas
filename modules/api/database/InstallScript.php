12a183d6ae4c96caf0f5e97dec2b9c27ab0e3cdf
Zach Mullen <zach.mullen@kitware.com>
2011-10-13 16:07:11 -0400

    ENH: refs #0292. When web api module is installed, creates default api keys
    for existing users.

--- modules/api/database/InstallScript.php
+++ modules/api/database/InstallScript.php
@@ -1,14 +1,46 @@
 <?php
+/*=========================================================================
+  MIDAS Server
 
+  Copyright (c) Kitware Inc. All rights reserved.
+  See Copyright.txt or http://www.Kitware.com/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+
+/**
+ * The install script for the api module
+ */
 class Api_InstallScript extends MIDASModuleInstallScript
   {
 
+  /**
+   * Pre-install callback does nothing
+   */
   public function preInstall()
     {
     }
 
+  /**
+   * Post-install callback creates default api keys
+   * for all existing users
+   */
   public function postInstall()
     {
+    include_once BASE_PATH.'/modules/api/models/AppModel.php';
+    $modelLoader = new MIDAS_ModelLoader();
+    $userModel = $modelLoader->loadModel('User');
+    $userapiModel = $modelLoader->loadModel('Userapi', 'api');
+
+    //limit this to 100 users; there shouldn't be very many when api is installed
+    $users = $userModel->getAll(false, 100, 'admin');
+    foreach($users as $user)
+      {
+      $userapiModel->createDefaultApiKey($user);
+      }
     }
   }
 
