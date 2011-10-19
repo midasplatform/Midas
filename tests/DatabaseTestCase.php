40a506256babc585ab12faaa2bf4ac70813a7731
Michael Grauer <michael.grauer@kitware.com>
2011-10-18 20:48:31 -0400

    ENH: refs #0297. Updating bitstream postgres seq ids from test data xml.

--- tests/DatabaseTestCase.php
+++ tests/DatabaseTestCase.php
@@ -94,6 +94,7 @@ abstract class DatabaseTestCase extends Zend_Test_PHPUnit_DatabaseTestCase
       $db->query("SELECT setval('item_item_id_seq', (SELECT MAX(item_id) FROM item)+1);");
       $db->query("SELECT setval('itemrevision_itemrevision_id_seq', (SELECT MAX(itemrevision_id) FROM itemrevision)+1);");
       $db->query("SELECT setval('folder_folder_id_seq', (SELECT MAX(folder_id) FROM folder)+1);");
+      $db->query("SELECT setval('bitstream_bitstream_id_seq', (SELECT MAX(bitstream_id) FROM bitstream)+1);");
       }
     }
 
