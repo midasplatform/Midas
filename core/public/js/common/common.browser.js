71cd013f13495c8951d4be9153e5c124adcede82
Yuzheng Zhou <yuzheng.zhou@kitware.com>
2011-10-26 13:08:16 -0400

    ENH: Refs #0299. Add a horizontal rule between global actions and foler/item specific actions. Remove the arbitrary horizontal rule among folder/item actions.

--- core/public/js/common/common.browser.js
+++ core/public/js/common/common.browser.js
@@ -204,7 +204,7 @@
       $('div.viewAction ul').fadeOut('fast',function()
       {
         $('div.viewAction ul').html('');
-        var html='';
+        var html='';   
         if(type=='community')
           {
             html+='<li><img alt="" src="'+json.global.coreWebroot+'/public/images/icons/view.png"/> <a href="'+json.global.webroot+'/community/'+element+'">'+json.browse.view+'</a></li>';            
@@ -216,7 +216,6 @@
             
             if(policy>=1)
               {
-              html+='<hr/>';
               html+='<li><img alt="" src="'+json.global.coreWebroot+'/public/images/FileTree/directory.png"/> <a onclick="createNewFolder('+element+');">'+json.browse.createFolder+'</a></li>';
               html+='<li><img alt="" src="'+json.global.coreWebroot+'/public/images/icons/upload.png"/> <a rel="'+json.global.webroot+'/upload/simpleupload/?parent='+element+'" class="uploadInFolder">'+json.browse.uploadIn+'</a></li>';
               html+='<li><img alt="" src="'+json.global.coreWebroot+'/public/images/icons/edit.png"/> <a onclick="editFolder('+element+');">'+json.browse.edit+'</a></li>';
@@ -233,7 +232,6 @@
             html+='<li><img alt="" src="'+json.global.coreWebroot+'/public/images/icons/download.png"/> <a href="'+json.global.webroot+'/download?items='+element+'">'+json.browse.downloadLastest+'</a></li>';
             if(policy>=1)
               {
-              html+='<hr/>';
               html+='<li><img alt="" src="'+json.global.coreWebroot+'/public/images/icons/share.png"/> <a  type="item" element="'+element+'" class="sharingLink">'+json.browse.share+'</a></li>';
               html+='<li class="removeItemLi"><img alt="" src="'+json.global.coreWebroot+'/public/images/icons/close.png"/> <a onclick="removeItem('+element+');">'+json.browse['removeItem']+'</a></li>'; 
               }   
