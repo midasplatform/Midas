d25b9451fc68cff1dff09634e7bf9388a1f45dc4
Zach Mullen <zach.mullen@kitware.com>
2011-10-25 21:30:53 -0400

    BUG: refs #0313. Fix failing pgsql test

--- modules/api/database/mysql/1.0.0.sql
+++ modules/api/database/mysql/1.0.0.sql
@@ -10,7 +10,7 @@ CREATE TABLE IF NOT EXISTS `api_userapi` (
   PRIMARY KEY (`userapi_id`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8 ;
 
-CREATE TABLE api_token (
+CREATE TABLE IF NOT EXISTS api_token (
   `token_id` bigint(20) NOT NULL AUTO_INCREMENT,
   `userapi_id` bigint(20) NOT NULL,
   `token` varchar(40) NOT NULL,
