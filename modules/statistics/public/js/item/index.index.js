cd48cb146c72584eac8028df9adc8e692385da6c
Zach Mullen <zach.mullen@kitware.com>
2011-11-09 11:34:04 -0500

    ENH: refs #0339. Add download maps widget (gmaps) to item statistics page

--- modules/statistics/public/js/item/index.index.js
+++ modules/statistics/public/js/item/index.index.js
@@ -1,22 +1,38 @@
-  $(document).ready(function() {
-
-    var errors = json.stats.downloads;
-    jQuery.each(errors, function(i, val) {
-      errors[i][1] = parseInt(errors[i][1]);
+$(document).ready(function() {
+  var tabs = $("#tabsGeneric").tabs({
+    select: function(event, ui) { }
     });
-
-    var plotErrors = $.jqplot('chartDownloads', [errors], {
-      title:'Number of downloads', 
-      gridPadding:{right:35},
-      axes:{
-        xaxis:{
-          renderer:$.jqplot.DateAxisRenderer, 
-          tickOptions:{formatString:'%b %#d'},
-          tickInterval:'1 day'
-        }
-      },
-      series:[{lineWidth:4, markerOptions:{style:'square'}}]
+  $('#tabsGeneric').show();
+  $('img.tabsLoading').hide();
+  $("#tabsGeneric").bind('tabsshow', function(event, ui) {
+    if (plotErrors._drawCount == 0) {
+      plotErrors.replot();
+      }
     });
-    
-    
+  var errors = json.stats.downloads;
+  jQuery.each(errors, function(i, val) {
+    errors[i][1] = parseInt(errors[i][1]);
   });
+
+  var plotErrors = $.jqplot('chartDownloads', [errors], {
+    title:'Number of downloads',
+    gridPadding:{right:35},
+    axes:{
+      xaxis:{
+        renderer:$.jqplot.DateAxisRenderer,
+        tickOptions:{formatString:'%b %#d'},
+        tickInterval:'1 day'
+      }
+    },
+    series:[{lineWidth:4, markerOptions:{style:'square'}}]
+  });
+
+  var latlng = new google.maps.LatLng(0, 0);
+  var myOptions = {
+    zoom: 2,
+    center: latlng,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  };
+  var map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
+  placeDownloadPins(map);
+});
