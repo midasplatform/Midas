61e669c9f075b04a0717f58bb3adc38499a32352
Yuzheng Zhou <yuzheng.zhou@kitware.com>
2011-11-09 06:02:34 -0500

    BUG: Refs #0271. Removed zend validator for firstname and lastname in installForm (server-side); removed zend validator for firstname, lastname, company, city and country in createAcountForm (server-side); added '+' as valid character in installForm validation (client-side); added '+' ad valid character in registerForm validataion (client-side)

--- core/public/js/install/install.step2.js
+++ core/public/js/install/install.step2.js
@@ -93,7 +93,7 @@
   
   function checkEmail(mailteste)
 {
-	var reg = new RegExp('^[a-z0-9]+([_|\.|-]{1}[a-z0-9]+)*@[a-z0-9]+([_|\.|-]{1}[a-z0-9]+)*[\.]{1}[a-z]{2,6}$', 'i');
+	var reg = new RegExp('^[a-z0-9]+([_|\.|-|\+]{1}[a-z0-9]+)*@[a-z0-9]+([_|\.|-]{1}[a-z0-9]+)*[\.]{1}[a-z]{2,6}$', 'i');
 
 	if(reg.test(mailteste))
 	{
