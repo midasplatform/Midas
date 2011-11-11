37934435ec8f58383b0e8c7e68936a7eec31a669
Michael Grauer <michael.grauer@kitware.com>
2011-11-10 22:07:22 -0500

    ENH: refs #0068. Added support for condor_dag and condor_dag_job to database.

--- modules/batchmake/database/mysql/0.1.0.sql
+++ modules/batchmake/database/mysql/0.1.0.sql
@@ -11,3 +11,21 @@ CREATE TABLE IF NOT EXISTS batchmake_itemmetric (
     bms_name character varying(256) NOT NULL,
     PRIMARY KEY (itemmetric_id)
 )   DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS condor_dag (
+    condor_dag_id bigint(20) NOT NULL AUTO_INCREMENT,
+    batchmake_task_id bigint(20) NOT NULL,
+    log_filename text NOT NULL,
+    PRIMARY KEY (condor_dag_id)
+)   DEFAULT CHARSET=utf8;
+
+
+CREATE TABLE IF NOT EXISTS condor_job (
+    condor_job_id bigint(20) NOT NULL AUTO_INCREMENT,
+    condor_dag_id bigint(20) NOT NULL,
+    jobdefinition_filename text NOT NULL,
+    output_filename text NOT NULL,
+    error_filename text NOT NULL,
+    log_filename text NOT NULL,
+    PRIMARY KEY (condor_job_id)
+)   DEFAULT CHARSET=utf8;
--- modules/batchmake/database/pgsql/0.1.0.sql
+++ modules/batchmake/database/pgsql/0.1.0.sql
@@ -10,3 +10,18 @@ CREATE TABLE batchmake_itemmetric (
     metric_name character varying(64) NOT NULL,
     bms_name character varying(256) NOT NULL
 );
+
+CREATE TABLE condor_dag (
+    condor_dag_id serial PRIMARY KEY,
+    batchmake_task_id bigint NOT NULL,
+    log_filename text NOT NULL
+);
+
+CREATE TABLE condor_job (
+    condor_job_id serial PRIMARY KEY,
+    condor_dag_id bigint NOT NULL,
+    jobdefinition_filename text NOT NULL,
+    output_filename text NOT NULL,
+    error_filename text NOT NULL,
+    log_filename text NOT NULL
+);
