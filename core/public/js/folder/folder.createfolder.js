2509441bde2141b85a1297d99f12a2d6765f0e4c
Yuzheng Zhou <yuzheng.zhou@kitware.com>
2011-11-23 14:18:59 -0500

    BUG: Refs #0366. Reload page when a top level folder is created.

--- core/public/js/folder/folder.createfolder.js
+++ core/public/js/folder/folder.createfolder.js
@@ -44,6 +44,11 @@ function successCreateFolder(responseText, statusText, xhr, form)
         {
         node.reload();
         }
+      // create a top level folder  
+      else
+        {
+        location.reload();  
+        }
     }
   else
     {
