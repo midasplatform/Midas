d1f7d8de5258eeb394985f754f1b6393b646f4cc
Charles Ma <charles.marion@kitware.com>
2011-12-08 14:41:29 +0100

    ENH: refs #233 Fixed Phmagick license

--- modules/thumbnailcreator/library/Phmagick/plugins/color.php
+++ modules/thumbnailcreator/library/Phmagick/plugins/color.php
@@ -1,22 +1,5 @@
 <?php
-/*=========================================================================
- MIDAS Server
- Copyright (c) Kitware SAS. 26 rue Louis Guérin. 69100 Villeurbanne, FRANCE
- All rights reserved.
- More information http://www.kitware.com
 
- Licensed under the Apache License, Version 2.0 (the "License");
- you may not use this file except in compliance with the License.
- You may obtain a copy of the License at
-
-         http://www.apache.org/licenses/LICENSE-2.0.txt
-
- Unless required by applicable law or agreed to in writing, software
- distributed under the License is distributed on an "AS IS" BASIS,
- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- See the License for the specific language governing permissions and
- limitations under the License.
-=========================================================================*/
 /*
     +--------------------------------------------------------------------------------------------+
     |   DISCLAIMER - LEGAL NOTICE -                                                              |
@@ -24,7 +7,7 @@
     |                                                                                            |
     |  This program is free for non comercial use, see the license terms available at            |
     |  http://www.francodacosta.com/licencing/ for more information                              |
-    |                                                                                            |    
+    |                                                                                            |
     |  This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; |
     |  without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. |
     |                                                                                            |
@@ -93,7 +76,7 @@ class phMagick_color {
         $p->setHistory($p->getDestination());
         return  $p ;
     }
-    
+
     /**
      * Convert's the image to grayscale
      */
@@ -115,7 +98,7 @@ class phMagick_color {
 		$cmd .= ' -sigmoidal-contrast '.$enhance.'x50%' ;
 		$cmd .= ' -background "none" "' . $p->getSource().'"' ;
 		$cmd .= ' "' . $p->getDestination() .'"';
-		
+
 		$p->execute($cmd);
 		$p->setSource($p->getDestination());
 		$p->setHistory($p->getDestination());
@@ -136,7 +119,7 @@ class phMagick_color {
         $p->setHistory($p->getDestination());
         return  $p ;
     }
-    
+
     function sepia(phmagick $p, $tone = 90){
 		$cmd   = $p->getBinary('convert');
 		$cmd .= ' -sepia-tone '. $tone .'% ' ;
@@ -149,18 +132,18 @@ class phMagick_color {
 	     $p->setHistory($p->getDestination());
 	    return  $p ;
     }
-    
+
 	function autoLevels(phmagick $p){
 		$cmd  = $p->getBinary('convert');
 		$cmd .= ' -normalize ' ;
 		$cmd .= ' -background "none" "' . $p->getSource().'"'  ;
 		$cmd .= ' "' . $p->getDestination() .'"' ;
-		
+
 		$p->execute($cmd);
 		$p->setSource($p->getDestination());
 		$p->setHistory($p->getDestination());
 		return  $p ;
 	}
-	
+
 }
 ?>
