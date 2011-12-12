099c6dd54eb102988735b046e1a653f3370a1307
Zach Mullen <zach.mullen@kitware.com>
2011-12-12 12:34:15 -0500

    BUG: refs #0370. Remove javascript calls from the upload applet
    -We now redirect to the uploaded files page when the java uploader finishes

--- core/controllers/UploadController.php
+++ core/controllers/UploadController.php
@@ -413,4 +413,10 @@ class UploadController extends AppController
       echo json_encode($info);
       }
     }//end saveuploaded
+
+  /** Link for the java applet to review uploaded files */
+  public function reviewAction()
+    {
+    $this->_redirect('/browse/uploaded');
+    }
 }//end class
