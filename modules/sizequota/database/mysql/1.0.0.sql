83f66727f27f11738029e7ad11ebae8faa442e94
Zach Mullen <zach.mullen@kitware.com>
2012-01-05 14:56:03 -0500

    ENH: refs #0377. Quota should be a varchar so it can be set to empty string

--- modules/sizequota/database/mysql/1.0.0.sql
+++ modules/sizequota/database/mysql/1.0.0.sql
@@ -2,7 +2,7 @@
 CREATE TABLE IF NOT EXISTS `sizequota_folderquota` (
   `folderquota_id` bigint(20) NOT NULL AUTO_INCREMENT,
   `folder_id` bigint(20) NOT NULL,
-  `quota` bigint(20) NOT NULL,
+  `quota` varchar(50) NOT NULL,
   PRIMARY KEY (`folderquota_id`),
   KEY `folder_id` (`folder_id`)
 ) DEFAULT CHARSET=utf8;
