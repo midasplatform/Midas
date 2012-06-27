214f631c7ea84d060cf3f604ac083ca92f2913ac
Patrick Reynolds <patrick.reynolds@kitware.com>
2012-06-27 16:37:01 -0400

    ENH: refs #490. Dashboard entry and manual extraction of DICOM.
    Users can now view the status of the dicomextractor plugin from the
    dashboard and manually run the dicom extraction from the item page.

--- modules/dicomextractor/controllers/components/ExtractorComponent.php
+++ modules/dicomextractor/controllers/components/ExtractorComponent.php
@@ -15,6 +15,54 @@ class Dicomextractor_ExtractorComponent extends AppComponent
 {
 
   /**
+   * Check whether a given application is configured properly.
+   * @param command the command to test with
+   * @param appName the human-readable application name
+   * @appendVersion whether we need the --version flag
+   * @return an array indicating whether the app is valid or not
+   */
+  private function getApplicationStatus($command, $appName,
+                                        $appendVersion = true)
+  {
+    $preparedCommand = str_replace("'", '"',$command);
+    if($appendVersion)
+      {
+      $preparedCommand .= ' --version';
+      }
+    exec($preparedCommand, $output, $return_var);
+    $parsedOutput = explode(' ', $output[0], 4);
+    $appVersion = $parsedOutput[2];
+    if($return_var !== 0)
+      {
+      return array(false,
+                   $appName . ' was not found or is not configured properly.');
+      }
+    else
+      {
+      return array(true, $appName . ' ' . $appVersion . ' is present.');
+      }
+  }
+
+  /**
+   * Verify that DCMTK is setup properly
+   */
+  public function isDCMTKWorking()
+  {
+    $ret = array();
+    $modulesConfig=Zend_Registry::get('configsModules');
+    $dcm2xmlCommand = $modulesConfig['dicomextractor']->dcm2xml;
+    $dcmftestCommand = $modulesConfig['dicomextractor']->dcmftest;
+    $dcmj2pnmCommand = $modulesConfig['dicomextractor']->dcmj2pnm;
+    $ret['dcm2xml'] = $this->getApplicationStatus($dcm2xmlCommand, 'dcm2xml');
+    $ret['dcmftest'] = $this->getApplicationStatus($dcmftestCommand,
+                                                   'dcmftest',
+                                                   false);
+    $ret['dcmj2pnm'] = $this->getApplicationStatus($dcmj2pnmCommand,
+                                                   'dcmj2pnm');
+    return $ret;
+  }
+
+  /**
    * Create a thumbnail from the series
    */
   public function thumbnail($item)
