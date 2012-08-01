9431ea63cc60a71c9a33a5a85a3c36f4cbf52f67
Zach Mullen <zach.mullen@kitware.com>
2012-08-01 14:11:52 -0400

    ENH: refs #393. Redirect to parent folder after item deletion

--- core/controllers/InstallController.php
+++ core/controllers/InstallController.php
@@ -281,7 +281,7 @@ class InstallController extends AppController
       $applicationConfig['global']['default.timezone'] = $form->getValue('timezone');
 
       $this->Component->Utility->createInitFile(BASE_PATH.'/core/configs/application.local.ini', $applicationConfig);
-      $this->_redirect("/admin?checkRecentItem=true#tabs-modules");
+      $this->_redirect("/admin#tabs-modules");
       }
     } // end method step2Action
 
