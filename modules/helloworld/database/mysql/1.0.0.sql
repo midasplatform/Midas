1b82fd1b835c93c8c6450e950defb5e7d491438a
Michael Grauer <michael.grauer@kitware.com>
2012-08-06 15:22:02 -0400

    ENH: refs #752. Removed helloworld module.

--- modules/helloworld/database/mysql/1.0.0.sql
+++
@@ -1,7 +0,0 @@
-
-CREATE TABLE IF NOT EXISTS `helloworld_hello` (
-  `hello_id` bigint(20) NOT NULL AUTO_INCREMENT,
-  PRIMARY KEY (`hello_id`)
-) DEFAULT CHARSET=utf8;
-
-INSERT INTO `helloworld_hello` (`hello_id`) VALUES (default(`hello_id`));
