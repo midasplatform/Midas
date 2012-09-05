a851169fdc5743a5108eca3fb28e4d8a3e76be65
Zach Mullen <zach.mullen@kitware.com>
2012-09-05 13:26:23 -0400

    ENH: refs #787. Add ability to extract and view center axial slice

--- modules/visualize/controllers/ParaviewController.php
+++ modules/visualize/controllers/ParaviewController.php
@@ -97,6 +97,100 @@ class Visualize_ParaviewController extends Visualize_AppController
         }
       }
 
+    if(!$userwebgl || $item->getSizebytes() > 1 * 1024 * 1024)
+      {
+      $this->view->renderer = 'js';
+      }
+    else
+      {
+      $this->view->renderer = 'webgl';
+      }
+    $this->view->json['visualize']['url'] = $filePath;
+    $this->view->json['visualize']['width'] = $this->_getParam('width');
+    $this->view->json['visualize']['height'] = $this->_getParam('height');
+    $this->view->width = $this->_getParam('width');
+    $this->view->height = $this->_getParam('height');
+    $this->view->fileLocation = $filePath;
+    $this->view->pwapp = $pwapp;
+    $this->view->usewebgl = $userwebgl;
+    $this->view->itemDao = $item;
+    $this->view->screenshotPath = '/data/visualize/_'.$item->getKey().'_1.png';
+    $this->view->useScreenshot = file_exists(BASE_PATH.$this->view->screenshotPath);
+    $this->view->loadState = $this->view->json['visualize']['openState'];
+    }
+
+  /** Use the slice view mode for volume data */
+  public function sliceAction()
+    {
+    $this->disableLayout();
+    $itemid = $this->_getParam('itemId');
+    $item = $this->Item->load($itemid);
+
+    if($item === false || !$this->Item->policyCheck($item, $this->userSession->Dao, MIDAS_POLICY_READ))
+      {
+      throw new Zend_Exception("This item doesn't exist  or you don't have the permissions.");
+      }
+
+    $modulesConfig = Zend_Registry::get('configsModules');
+    $paraviewworkdir = $modulesConfig['visualize']->paraviewworkdir;
+    $customtmp = $modulesConfig['visualize']->customtmp;
+    $useparaview = $modulesConfig['visualize']->useparaview;
+    $userwebgl = $modulesConfig['visualize']->userwebgl;
+    $usesymlinks = $modulesConfig['visualize']->usesymlinks;
+    $pwapp = $modulesConfig['visualize']->pwapp;
+    if(!isset($useparaview) || !$useparaview)
+      {
+      throw new Zend_Exception('Please enable the use of a ParaViewWeb server on the module configuration page');
+      }
+
+    if(!isset($paraviewworkdir) || empty($paraviewworkdir))
+      {
+      throw new Zend_Exception('Please set the ParaView work directory');
+      }
+
+
+    $pathArray = $this->ModuleComponent->Main->createParaviewPath();
+    $path = $pathArray['path'];
+    $tmpFolderName = $pathArray['foderName'];
+
+    $revision = $this->Item->getLastRevision($item);
+    $bitstreams = $revision->getBitstreams();
+    foreach($bitstreams as $bitstream)
+      {
+      if($usesymlinks)
+        {
+        symlink($bitstream->getFullPath(), $path.'/'.$bitstream->getName());
+        }
+      else
+        {
+        copy($bitstream->getFullPath(), $path.'/'.$bitstream->getName());
+        }
+
+      $ext = strtolower(substr(strrchr($bitstream->getName(), '.'), 1));
+      if($ext != 'pvsm')
+        {
+        $filePath = $paraviewworkdir."/".$tmpFolderName.'/'.$bitstream->getName();
+        $mainBitstream = $bitstream;
+        }
+      }
+
+    $this->view->json['visualize']['openState'] = false;
+
+    foreach($bitstreams as $bitstream)
+      {
+      $ext = strtolower(substr(strrchr($bitstream->getName(), '.'), 1));
+      if($ext == 'pvsm')
+        {
+        $file_contents = file_get_contents($path.'/'.$bitstream->getName());
+        $file_contents = preg_replace('/\"([a-zA-Z0-9_.\/\\\:]{1,1000})'.  str_replace('.', '\.', $mainBitstream->getName())."/", '"'.$filePath, $file_contents);
+        $filePath = $paraviewworkdir."/".$tmpFolderName.'/'.$bitstream->getName();
+        $inF = fopen($path.'/'.$bitstream->getName(), "w");
+        fwrite($inF, $file_contents);
+        fclose($inF);
+        $this->view->json['visualize']['openState'] = true;
+        break;
+        }
+      }
 
     if(!$userwebgl || $item->getSizebytes() > 1 * 1024 * 1024)
       {
@@ -120,6 +214,6 @@ class Visualize_ParaviewController extends Visualize_AppController
     $this->view->loadState = $this->view->json['visualize']['openState'];
     }
 
-
 } // end class
 ?>
+
