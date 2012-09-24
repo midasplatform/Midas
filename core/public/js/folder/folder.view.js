76f88540251493889bbe86d8dcede22ecb4750bc
Zach Mullen <zach.mullen@kitware.com>
2012-09-24 18:22:31 -0400

    ENH: refs #822. Various improvements to the action menus

--- core/public/js/folder/folder.view.js
+++ core/public/js/folder/folder.view.js
@@ -2,45 +2,41 @@ var midas = midas || {};
 
 $(document).ready(function() {
 
-  $("#browseTable").treeTable({
-    onFirstInit: midas.enableRangeSelect,
-    onNodeShow: midas.enableRangeSelect,
-    onNodeHide: midas.enableRangeSelect
-  });
-  /**
-   * Non-ajax'd pages (ones with only items in the view) do not get their
-   * init callback made, so we manually initialize range selection
-   * before the items are made visible asynchronously
-   */
-  $('input.treeCheckbox').enableCheckboxRangeSelection({
-    onRangeSelect: function() {
-      midas.genericCallbackCheckboxes($('#browseTable'));
-      }
+    $("#browseTable").treeTable({
+        onFirstInit: midas.enableRangeSelect,
+        onNodeShow: midas.enableRangeSelect,
+        onNodeHide: midas.enableRangeSelect
+    });
+    /**
+     * Non-ajax'd pages (ones with only items in the view) do not get their
+     * init callback made, so we manually initialize range selection
+     * before the items are made visible asynchronously
+     */
+    $('input.treeCheckbox').enableCheckboxRangeSelection({
+        onRangeSelect: function () {
+            midas.genericCallbackCheckboxes($('#browseTable'));
+        }
     });
 
-  /**
-   * Select/deslect all rows. If we are doing deselect all, we include hidden
-   * ones
-   */
-  midas.browser.enableSelectAll();
+    /**
+     * Select/deslect all rows. If we are doing deselect all, we include hidden
+     * ones
+     */
+    midas.browser.enableSelectAll();
 
-  $("img.tableLoading").hide();
-  $("table#browseTable").show();
-  midas.genericCallbackSelect($('div.defaultSideTrigger'));
+    $("img.tableLoading").hide();
+    $("table#browseTable").show();
 
-  $( "#tabsGeneric" ).tabs();
-  $("#tabsGeneric").show();
+    $( "#tabsGeneric" ).tabs();
+    $("#tabsGeneric").show();
 
-  if($('.pathBrowser li').length > 5)
-    {
-    while($('.pathBrowser li').length > 5)
-      {
-      $('.pathBrowser li:first').remove();
-      }
-
-    $('.pathBrowser li:first').before('<li><span>...</span></li>');
+    if($('.pathBrowser li').length > 5) {
+        while($('.pathBrowser li').length > 5) {
+            $('.pathBrowser li:first').remove();
+        }
+        $('.pathBrowser li:first').before('<li><span>...</span></li>');
     }
-  $('.pathBrowser').show();
+    $('.pathBrowser').show();
 });
 
 //dependance: common/browser.js
