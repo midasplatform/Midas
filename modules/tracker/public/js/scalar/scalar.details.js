55b5594c026883d708df4b9f8e6ebda7840e5d6e
Zach Mullen <zach.mullen@kitware.com>
2012-10-15 12:28:21 -0400

    ENH: refs #845. Scalar details page

---
+++ modules/tracker/public/js/scalar/scalar.details.js
@@ -0,0 +1,30 @@
+$('#deleteScalar').click(function () {
+    var html = "Are you sure you want to delete this scalar value?";
+    html += '<div style="float: right; margin-top: 15px;">';
+    html += '<input type="button" style="margin-left: 0px;" class="globalButton" id="deleteScalarYes" value="Yes" />';
+    html += '<input type="button" style="margin-left: 10px;" class="globalButton" id="deleteScalarNo" value="Cancel" />';
+    html += '<input type="hidden" class="scalarIdChild" value="'+$('input.scalarId').val()+'" />';
+    html += '</div>';
+    midas.showDialogWithContent('Confirm delete scalar', html, false);
+    $('#deleteScalarYes').click(function () {
+        $.post(json.global.webroot+'/tracker/scalar/delete', {scalarId: $('input.scalarIdChild').val() },
+            function(data) {
+                var jsonResponse = jQuery.parseJSON(data);
+                if(jsonResponse == null) {
+                    midas.createNotice('Error', 2000, 'error');
+                    return;
+                }
+                if(jsonResponse.status == 'ok') {
+                    window.location.reload();
+                    return;
+                }
+                else {
+                    midas.createNotice(jsonResponse.message, 3000, jsonResponse.status);
+                }
+            }
+        );
+    });
+    $('#deleteScalarNo').click(function () {
+        $('div.MainDialog').dialog('close');
+    });
+});
