d4e607e6164de598db2348f6f996850e35370d75
Yuzheng Zhou <yuzheng.zhou@kitware.com>
2012-10-17 14:33:09 -0400

    ENH: Refs #0850. Added api methods to call scripts directly within plugin.

--- modules/dicomuploader/controllers/forms/ConfigForm.php
+++ modules/dicomuploader/controllers/forms/ConfigForm.php
@@ -24,13 +24,18 @@ class Dicomuploader_ConfigForm extends AppForm
 
     $dcm2xml = new Zend_Form_Element_Text('dcm2xml');
     $storescp = new Zend_Form_Element_Text('storescp');
-    $tmpdir = new Zend_Form_Element_Text('tmpdir');
-    $tmpdir->setRequired(true)
+    $storescp_port = new Zend_Form_Element_Text('storescp_port');
+    $storescp_study_timeout = new Zend_Form_Element_Text('storescp_study_timeout');
+    $receptiondir = new Zend_Form_Element_Text('receptiondir');
+    $receptiondir->setRequired(true)
            ->addValidator('NotEmpty', true);
+    $pydas_dest_folder = new Zend_Form_Element_Text('pydas_dest_folder');
     $submit = new  Zend_Form_Element_Submit('submitConfig');
     $submit ->setLabel('Save configuration');
 
-    $form->addElements(array($dcm2xml, $storescp, $tmpdir, $submit));
+    $form->addElements(array($dcm2xml, $storescp, $storescp_port,
+       $storescp_study_timeout, $receptiondir, $pydas_dest_folder,
+       $submit));
     return $form;
     }
 
--- modules/dicomuploader/library/uploaderWapper.py
+++ modules/dicomuploader/library/uploaderWapper.py
@@ -1,38 +1,98 @@
+#!/usr/bin/python
+
 import os
+import sys
+import getopt
+import signal
+
 from DICOMHandler import DICOMListener
 
+class Usage(Exception):
+    def __init__(self, msg):
+        self.msg = msg
 
-if __name__ == '__main__':
-    # read the config file for DICOM uploader
-    config_file = open('uploader.cfg')
-    config = {}
-    for line in config_file:
-        line = line.strip()
-        if line is not None and line != '':
-            cols = line.split('=')
-            config[cols[0]] = cols[1]
-    config_file.close()
+def killUploader(application, cmd):
+    for line in os.popen("ps ax | grep %s" % cmd):
+        fields = line.split()
+        pid = fields[0]
+        process = fields[4]
+        shell_called_process = ''
+        if len(fields) > 6:
+            shell_called_process = fields[6]
+        if process.find('grep') == 0:
+            continue
+        elif process.find('python') == 0 or process.find('storescp') == 0 or shell_called_process.find('storescp') == 0:
+            #Kill the Process. Change signal.SIGHUP to signal.SIGKILL if you like
+            os.kill(int(pid), signal.SIGHUP)
 
-    if 'storescp_dir' not in config or not os.path.isdir(config['storescp_dir']):
-        print "You must specify storescp's output directory (storescp_dir)  in uploader.cfg"
-        exit(1)
-    if 'storescp_exe' not in config:
-        config['storescp_exe'] = 'scorescp'
-    if 'storescp_port' not in config:
-        config['storescp_port'] = 55555
-    if 'storescp_study_timeout' not in config:
-        config['storescp_study_timeout'] = 30
+def main():
+    """
+    Wrapper function to start and stop storescp listener
+    """
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], "hros:p:t:k:c:i:u:e:a:d:", ["help", "start", "stop", \
+            "storescp=", "port=", "timeout=", "scriptpath=", "dcm2xml=", "incoming=", \
+            "url=", "email=", "apikey=", "dest="])
+    except getopt.error, msg:
+        raise Usage(msg)
+    start = False
+    url = ''
+    user_email = ''
+    apikey = ''
+    dest_folder = ''
+    incoming_dir = ''
+    storescp_cmd = ''
+    script_path = ''
+    dcm2xml_cmd = ''
+    storescp_port = ''
+    storescp_timeout = ''
+    for opt, arg in opts:
+        if opt in ('-h', "--help"):
+            sample = 'uploader.py --start -s <storescp_cmd> ' \
+              '-p <storescp_port> -t <storescp_studay_timeout> ' \
+              '-k <script_path> -c <dcm2xml_cmd> -i <incoming_dir> ' \
+              '-u <midas_url> -e <midas_user_email> ' \
+              '-a <midas_api_key> -d <midas_destination_folder>'
+            print sample
+            sys.exit()
+        elif opt in ("-r", "--start"):
+            start = True
+        elif opt in ("-o", "--stop"):
+            start = False
+        elif opt in ("-s", "--storescp"):
+            storescp_cmd = arg
+        elif opt in ("-p", "--port"):
+            storescp_port = arg
+        elif opt in ("-t", "--timeout"):
+            storescp_timeout = arg
+        elif opt in ("-k", "--scriptpath"):
+            script_path = arg
+        elif opt in ("-c", "--dcm2xml"):
+            dcm2xml_cmd = arg
+        elif opt in ("-i", "--incoming"):
+            incoming_dir = arg
+        elif opt in ("-u", "--url"):
+            url = arg
+        elif opt in ("-e", "--email"):
+            user_email = arg
+        elif opt in ("-a", "--apikey"):
+            apikey = arg
+        elif opt in ("-d", "--dest"):
+            dest_folder = arg
+    # start/stop storescup listener
+    myListener = DICOMListener()
+    if start:
+        # callback command used by storescp '--eostudy-timeout' option
+        callback_cmd = "'python %s -c %s -i %s -u %s -e %s -a %s -d %s'" % ( \
+          script_path, dcm2xml_cmd, incoming_dir, url, user_email, apikey, dest_folder)
+        myListener.start(incoming_dir, callback_cmd, \
+          storescp_cmd, storescp_port, storescp_timeout)
+    else:
+        if not storescp_cmd:
+            storescp_cmd = 'storescp'
+        app_name = 'uploaderWapper'
+        killUploader(app_name, storescp_cmd)
 
-    if 'midas_destination' not in config:
-        config['midas_destination'] = 'Public'
+if __name__ == "__main__":
+    main()
 
-    # TOCHANGE: call uploader.py with storescp_dir as the callback_cmd
-    callback_cmd = "'python uploader.py --email=%s --url=%s --apikey=%s --destination=%s --incomingdir=%s'" % ( \
-      config['midas_email'], config['midas_url'], config['midas_api_key'], config['midas_destination'], config['storescp_dir'])
-
-
-    # Start storescup listener
-    myListener = DICOMListener(config['storescp_dir'], \
-      callback_cmd, config['storescp_exe'],\
-      config['storescp_port'], config['storescp_study_timeout'])
-    myListener.start();
