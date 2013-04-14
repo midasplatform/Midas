46dc4eb5cbb56dd1ad1a5119614f97d80a7c6d66
Zach Mullen <zach.mullen@kitware.com>
2013-04-14 16:05:56 -0400

    BUG: refs #1001. Remove on update clause of mysql date_update columns

---
+++ core/database/upgrade/3.2.14.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * Upgrade 3.2.14 fixes bug #1001: folder modified time changed incorrectly
+ */
+class Upgrade_3_2_14 extends MIDASUpgrade
+{
+
+  public function preUpgrade()
+    {
+    }
+
+  public function mysql()
+    {
+    // Remove "on update current timestamp" qualifier from the date_update column
+    $this->db->query("ALTER TABLE `folder`
+      CHANGE `date_update` `date_update` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP");
+    $this->db->query("ALTER TABLE `item`
+      CHANGE `date_update` `date_update` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP");
+    $this->db->query("ALTER TABLE `itemrevision`
+      CHANGE `date` `date` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP");
+    $this->db->query("ALTER TABLE `bitstream`
+      CHANGE `date` `date` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP");
+    }
+
+  public function pgsql()
+    {
+    // bug only existed in our mysql table defs, NOP for pgsql
+    }
+
+  public function postUpgrade()
+    {
+    }
+}
+?>
