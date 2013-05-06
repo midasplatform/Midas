7d562c125b9ce6b33b53404ea7f72f2889c183bc
Zach Mullen <zach.mullen@kitware.com>
2013-05-06 13:31:51 -0400

    ENH: refs #1014. More granular repository browser URL settings
    -Move them to the producer scope

---
+++ modules/tracker/database/upgrade/1.0.2.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * Adds revision URL as a configuration parameter on producers
+ */
+class Tracker_Upgrade_1_0_2 extends MIDASUpgrade
+{
+  public function preUpgrade()
+    {
+    }
+
+  public function mysql()
+    {
+    $this->db->query("ALTER TABLE `tracker_producer` ADD COLUMN `revision_url` text NOT NULL DEFAULT ''");
+    }
+
+  public function pgsql()
+    {
+    $this->db->query("ALTER TABLE tracker_producer ADD COLUMN revision_url text NOT NULL DEFAULT ''");
+    }
+
+  public function postUpgrade()
+    {
+    }
+}
+?>
