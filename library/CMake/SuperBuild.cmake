63514d00bcb6af018169bb267079ae3c500fffa1
Jamie Snape <jamie.snape@kitware.com>
2014-06-11 14:56:00 -0400

    Add or update file headers

--- library/CMake/SuperBuild.cmake
+++ library/CMake/SuperBuild.cmake
@@ -1,25 +1,22 @@
-##############################################################################
-#
-# Library:   Midas
-#
-# Copyright 2010 Kitware Inc. 28 Corporate Drive,
-# Clifton Park, NY, 12065, USA.
-#
+#=============================================================================
+# MIDAS Server
+# Copyright (c) Kitware SAS. 26 rue Louis Guérin. 69100 Villeurbanne, FRANCE
 # All rights reserved.
+# More information http://www.kitware.com
 #
-# Licensed under the Apache License, Version 2.0 ( the "License" );
+# Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#       http://www.apache.org/licenses/LICENSE-2.0
+#         http://www.apache.org/licenses/LICENSE-2.0.txt
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
-##############################################################################
+#=============================================================================
+
 include( ExternalProject )
 
 set( base "${CMAKE_BINARY_DIR}" )
