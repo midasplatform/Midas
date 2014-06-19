cfae6825ab7a325ad75ed82f45ceb4d746a3ac88
Jamie Snape <jamie.snape@kitware.com>
2014-06-19 14:17:22 -0400

    Tidy index page and do not require log directory to be writable

--- index.php
+++ index.php
@@ -18,10 +18,16 @@
  limitations under the License.
 =========================================================================*/
 
+define('BASE_PATH', realpath(dirname(__FILE__)));
+
+set_include_path('.'
+  . PATH_SEPARATOR . './library'
+  . PATH_SEPARATOR . './core/dao/'
+  . PATH_SEPARATOR . get_include_path());
+
 if(function_exists('apache_get_modules'))
   {
-  $modules = apache_get_modules();
-  $mod_rewrite = in_array('mod_rewrite', $modules);
+  $mod_rewrite = in_array('mod_rewrite', apache_get_modules());
   }
 else
   {
@@ -31,31 +37,20 @@ else
 if(!$mod_rewrite)
   {
   echo "Please install/enable the module rewrite";
-  exit;
-  }
-error_reporting(E_ALL | E_STRICT);
-ini_set('display_errors', 'on');
-set_include_path('.'
-  . PATH_SEPARATOR . './library'
-  . PATH_SEPARATOR . './core/dao/'
-  . PATH_SEPARATOR . get_include_path());
-
-define('BASE_PATH', realpath(dirname(__FILE__)));
-
-if(!is_writable(BASE_PATH."/core/configs") || !is_writable(BASE_PATH."/log") || !is_writable(BASE_PATH."/data") || !is_writable(BASE_PATH."/tmp"))
-  {
-  echo "To use Midas, the following repertories have to be writable by apache:
-        <ul>
-          <li>".BASE_PATH."/core/configs</li>
-          <li>".BASE_PATH."/log</li>
-          <li>".BASE_PATH."/data</li>
-          <li>".BASE_PATH."/tmp</li>
-        </ul>";
   exit();
   }
 
-// remember me cookie lifetime
-//ini_set('session.gc_maxlifetime', 60 * 60 * 24 * 15); //15 days
+if(!is_writable(BASE_PATH . '/core/configs') || !is_writable(BASE_PATH . '/data') || !is_writable(BASE_PATH . '/tmp'))
+  {
+  echo 'To use Midas Platform, the following folders must be writable by your web server:
+        <ul>
+          <li>' . BASE_PATH . '/core/configs</li>
+          <li>' . BASE_PATH . '/data</li>
+          <li>' . BASE_PATH . '/tmp</li>
+        </ul>';
+  exit();
+  }
+
 define('START_TIME', microtime(true));
 
 require_once 'Zend/Loader/Autoloader.php';
@@ -64,10 +59,7 @@ require_once 'Zend/Application.php';
 $loader = Zend_Loader_Autoloader::getInstance();
 $loader->registerNamespace('App_');
 
-require_once(BASE_PATH . "/core/include.php");
+require_once(BASE_PATH . '/core/include.php');
 
-// Create application, bootstrap, and run
 $application = new Zend_Application('global', CORE_CONFIG);
-
-$application->bootstrap()
-->run();
+$application->bootstrap()->run();
