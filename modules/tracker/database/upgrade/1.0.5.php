543c4f0d8e16893b7f451623275f106ba0396ad5
Zach Mullen <zach.mullen@kitware.com>
2014-07-18 10:49:05 -0400

    Add blob fields for extra params and URLs

---
+++ modules/tracker/database/upgrade/1.0.5.php
@@ -0,0 +1,46 @@
+<?php
+/*=========================================================================
+ MIDAS Server
+ Copyright (c) Kitware SAS. 26 rue Louis Guérin. 69100 Villeurbanne, FRANCE
+ All rights reserved.
+ More information http://www.kitware.com
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+         http://www.apache.org/licenses/LICENSE-2.0.txt
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+=========================================================================*/
+
+/**
+ * Adds branch name to scalar submission table, and adds an index for it.
+ */
+class Tracker_Upgrade_1_0_5 extends MIDASUpgrade
+  {
+  public function preUpgrade()
+    {
+    }
+
+  public function mysql()
+    {
+    $this->db->query("ALTER TABLE `tracker_scalar` ADD COLUMN `params` text NULL DEFAULT NULL");
+    $this->db->query("ALTER TABLE `tracker_scalar` ADD COLUMN `extra_urls` text NULL DEFAULT NULL");
+
+    }
+
+  public function pgsql()
+    {
+    $this->db->query("ALTER TABLE tracker_scalar ADD COLUMN params text NULL DEFAULT NULL");
+    $this->db->query("ALTER TABLE tracker_scalar ADD COLUMN extra_urls text NULL DEFAULT NULL");
+    }
+
+  public function postUpgrade()
+    {
+    }
+  }
