7dca72ebef7c40f92fe19d77885d331f6a059e6b
Jamie Snape <jamie.snape@kitware.com>
2015-09-30 11:42:53 -0400

    Remove "example" module

--- modules/example/database/sqlite/1.0.0.sql
+++
@@ -1,9 +0,0 @@
--- Midas Server. Copyright Kitware SAS. Licensed under the Apache License 2.0.
-
--- SQLite database for the example module, version 1.0.0
-
-CREATE TABLE IF NOT EXISTS "example_wallet" (
-  "example_wallet_id" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-  "user_id" INTEGER NOT NULL,
-  "dollars" INTEGER NOT NULL
-);
