be8693f39f827e0b0666f9a6391f1e3e1be9c068
mgrauer <michael.grauer@kitware.com>
2015-10-01 19:50:37 +0000

    Cleave tracker_scalar param to tracker_param

---
+++ modules/tracker/database/upgrade/1.2.2.php
@@ -0,0 +1,56 @@
+<?php
+/*=========================================================================
+ Midas Server
+ Copyright Kitware SAS, 26 rue Louis Guérin, 69100 Villeurbanne, France.
+ All rights reserved.
+ For more information visit http://www.kitware.com/.
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+         http://www.apache.org/licenses/LICENSE-2.0.txt
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+=========================================================================*/
+
+/**
+ * Upgrade the tracker module to version 1.2.2.
+ */
+class Tracker_Upgrade_1_2_2 extends MIDASUpgrade
+{
+    /** Upgrade a MySQL database. */
+    public function mysql()
+    {
+        $this->db->query(
+            'CREATE TABLE IF NOT EXISTS `tracker_param` ('.
+            '    `param_id` bigint(20) NOT NULL AUTO_INCREMENT,'.
+            '    `scalar_id` bigint(20) NOT NULL,'.
+            '    `param_name` varchar(255) NOT NULL,'.
+            '    `param_type` enum(\'text\', \'numeric\') NOT NULL,'.
+            '    `text_value` text,'.
+            '    `numeric_value` double,'.
+            '    PRIMARY KEY (`param_id`),'.
+            '    KEY (`param_name`)'.
+            ') DEFAULT CHARSET=utf8;');
+    }
+
+    /** Upgrade a PostgreSQL database. */
+    public function pgsql()
+    {
+        $this->db->query(
+            'CREATE TABLE IF NOT EXISTS "tracker_param" ('.
+            '    "param_id" serial PRIMARY KEY,'.
+            '    "scalar_id" bigint NOT NULL,'.
+            '    "param_name" character varying(255) NOT NULL,'.
+            '    "param_type" text CHECK (param_type in (\'text\', \'numeric\')),'.
+            '    "text_value" text,'.
+            '    "numeric_value" double precision);'
+        );
+        $this->db->query('CREATE INDEX "tracker_param_param_name_idx" on "tracker_param" ("param_name");');
+    }
+}
---
+++ modules/tracker/models/base/ParamModelBase.php
@@ -0,0 +1,49 @@
+<?php
+/*=========================================================================
+ Midas Server
+ Copyright Kitware SAS, 26 rue Louis Guérin, 69100 Villeurbanne, France.
+ All rights reserved.
+ For more information visit http://www.kitware.com/.
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+         http://www.apache.org/licenses/LICENSE-2.0.txt
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+=========================================================================*/
+
+/** Param base model class for the tracker module. */
+abstract class Tracker_ParamModelBase extends Tracker_AppModel
+{
+    /** Constructor. */
+    public function __construct()
+    {
+        parent::__construct();
+
+        $this->_name = 'tracker_param';
+        $this->_key = 'param_id';
+        $this->_mainData = array(
+            'param_id' => array('type' => MIDAS_DATA),
+            'scalar_id' => array('type' => MIDAS_DATA),
+            'param_name' => array('type' => MIDAS_DATA),
+            'param_type' => array('type' => MIDAS_DATA),
+            'text_value' => array('type' => MIDAS_DATA),
+            'numeric_value' => array('type' => MIDAS_DATA),
+            'scalar' => array(
+                'type' => MIDAS_MANY_TO_ONE,
+                'model' => 'Scalar',
+                'module' => $this->moduleName,
+                'parent_column' => 'scalar_id',
+                'child_column' => 'scalar_id',
+            ),
+        );
+
+        $this->initialize();
+    }
+}
