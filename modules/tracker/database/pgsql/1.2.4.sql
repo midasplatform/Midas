35254f107573cd3328da3cf891a3fbc0a6a29b69
mgrauer <michael.grauer@kitware.com>
2016-03-08 18:15:44 +0000

    Correct tracker_user2aggregate_metric_spec table definition

--- modules/tracker/database/pgsql/1.2.4.sql
+++ modules/tracker/database/pgsql/1.2.4.sql
@@ -119,7 +119,6 @@ CREATE INDEX "tracker_aggregate_metric_spec_branch" ON "tracker_aggregate_metric
 CREATE TABLE IF NOT EXISTS "tracker_user2aggregate_metric_spec" (
     "id" serial PRIMARY KEY,
     "user_id"  bigint NOT NULL,
-    "aggregate_metric_spec_id" bigint NOT NULL
+    "aggregate_metric_spec_id" bigint NOT NULL,
+    UNIQUE ("user_id", "aggregate_metric_spec_id")
 );
-
-CREATE INDEX "tracker_user2aggregate_metric_spec_ams_id_idx" ON "tracker_user2aggregate_metric_spec" ("aggregate_metric_spec_id");
